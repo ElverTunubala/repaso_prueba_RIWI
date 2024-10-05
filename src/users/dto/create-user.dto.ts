4e8a93264cef4107a6f67ad0ef00cbdba5ff28d9
ElverTunubala <elvertunubala@gmail.com>
2024-10-05 16:42:49 -0600

    add swagger, validation pipes and update auth

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,7 +2,6 @@ import { IsNumber } from 'class-validator';
 import { RegisterAuthDto } from 'src/auth/dto/register.dto';
 
 export class CreateUserDto extends RegisterAuthDto {
-
     @IsNumber()
     public id: string;
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,4 @@
-
+import { Exclude } from 'class-transformer';
 import { Role } from 'src/roles/roles.enum';
 import {Entity,Column,PrimaryGeneratedColumn} from 'typeorm';
 
@@ -10,7 +10,8 @@ export class UserEntity {
 
     @Column()
     email: string;
-  
+
+    @Exclude()
     @Column()
     password: string;
     
